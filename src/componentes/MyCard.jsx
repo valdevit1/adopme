1fb6e254d78e572c941d13ea05fd5b67073df47c
mario <mario_valdebenito@hotmail.com>
2023-11-18 19:24:22 -0600

    primer commit

--- src/componentes/MyCard.jsx
+++ src/componentes/MyCard.jsx
@@ -4,11 +4,13 @@ import Tags from "./Tags";
 function MyCard(props) {
   return (
     <>
-      <Card style={{ width: "18rem" }}>
+      <Card style={{ width: "18rem", marginRight: "1rem" }}>
         <Card.Img variant="top" src={props.imagen} />
         <Card.Body>
           <Card.Title>{props.nombre}</Card.Title>
-          <Card.Text>{props.descripcion}</Card.Text>
+          <Card.Text style={{ fontSize: "12px" }}>
+            {props.descripcion}
+          </Card.Text>
           <Tags color={props.color} tagsnombre={props.nombre} />
         </Card.Body>
       </Card>
--- src/componentes/Tags.jsx
+++ src/componentes/Tags.jsx
@@ -1,7 +1,12 @@
 import Badge from "react-bootstrap/Badge";
 
 function Tags(props) {
-  return <Badge bg={props.color}>{props.tagsnombre}</Badge>;
+  return (
+    <Badge bg={props.color}>
+      {props.tagsnombre}
+      {props.nombre}
+    </Badge>
+  );
 }
 
 export default Tags;
